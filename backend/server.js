3ab8db7744d49aeedb066cec867fb90ce0057798
PerdanaAnja <191240000940@unisnu.ac.id>
2022-06-15 07:24:29 +0700

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const res = require('express/lib/response')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(port, () => console.log('Server started on port', port))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
