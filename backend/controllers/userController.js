b8d465844f0ed5594cf1f3bac8cd9ce138d8a5f0
PerdanaAnja <191240000940@unisnu.ac.id>
2022-07-12 10:10:32 +0700

    protectingroutes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,7 +72,13 @@ const loginUser = asyncHandler(async(req,res) => {
 // @route POST /api/users/login
 // @access Public
 const getMe = asyncHandler(async(req,res) => {
-    res.json({message: "User data display"})
+    const { _id, name, email} = await User.findById(req.user.id);
+
+    res.status(200).json({
+        id: _id,
+        name,
+        amail,
+    }):
 })
 
 // generate JWT
